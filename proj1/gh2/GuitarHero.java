63da6567f2f18e0d639aa72102fe7a668728cc13
Achilles <jiangyongjian01@gmail.com>
2024-06-19 22:26:05 +0800

    完成GuitarHero代码

---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,51 @@
+package gh2;
+import deque.Deque;
+import deque.LinkedListDeque;
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class GuitarHero {
+
+
+    private static Deque<Double> CONCERT = new LinkedListDeque<>();
+    private static final int totalCONCERT = 37;
+    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    private static GuitarString[] strings = new GuitarString[totalCONCERT];
+
+    public static void getStart() {
+        double currentCONCERT;
+        for(int i = 0; i < totalCONCERT; i++) {
+            currentCONCERT = 440.0 * Math.pow(2, (i - 24.0) / 12.0);
+            CONCERT.addLast(currentCONCERT);
+            strings[i] = new GuitarString(currentCONCERT);
+        }
+    }
+
+    public static void main(String[] args) {
+        getStart();
+        while(true) {
+
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int currentIndex = keyboard.indexOf(key);
+                if(currentIndex != -1) {
+                    strings[currentIndex].pluck();
+                }
+            }
+
+            double sample = 0.0;
+            for(int i = 0; i < totalCONCERT; i++) {
+                sample += strings[i].sample();
+            }
+            StdAudio.play(sample);
+
+            for(int i = 0; i < totalCONCERT; i++) {
+                strings[i].tic();
+            }
+
+        }
+
+    }
+
+
+}
