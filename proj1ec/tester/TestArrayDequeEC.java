1ac01312ccc194fcb45c4ff058e89a69aebe84ec
Achilles <jiangyongjian01@gmail.com>
2024-06-22 15:16:46 +0800

    修复：应该输出完整操作序列而不是单独的一行报错

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -7,8 +7,10 @@ import student.StudentArrayDeque;
 
 public class TestArrayDequeEC {
 
+
     StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
     ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+    String operationSequence = "";
 
     @Test
     public void test1() {
@@ -17,34 +19,34 @@ public class TestArrayDequeEC {
             if (optionalNum == 0) {
                 sad.addFirst(i);
                 ads.addFirst(i);
+                operationSequence += "addFirst(" + i + ")\n";
             } else {
                 sad.addLast(i);
                 ads.addLast(i);
+                operationSequence += "addLast(" + i + ")\n";
             }
         }
 
-        for (int i = 0; i < 999; i++) {  // Here, change to 999 as mentioned in the problem description
+        for (int i = 0; i < 1000; i++) {  // Here, change to 999 as mentioned in the problem description
             int optionalNum = StdRandom.uniform(2);
             if (optionalNum == 0) {
-                    Integer sadRemoved = sad.removeFirst();
-                    Integer adsRemoved = ads.removeFirst();
-                    //System.out.println("sad.removeFirst() -> " + sadRemoved);
-                    //System.out.println("ads.removeFirst() -> " + adsRemoved);
-                    //System.out.println();
-                    assertEquals("Mismatch at removeFirst()", adsRemoved, sadRemoved);
+                Integer sadRemoved = sad.removeFirst();
+                Integer adsRemoved = ads.removeFirst();
+                operationSequence += "removeFirst()\n";
+                assertEquals(operationSequence, adsRemoved, sadRemoved);
             } else {
                 Integer sadRemoved = sad.removeLast();
                 Integer adsRemoved = ads.removeLast();
-                //System.out.println("sad.removeFirst() -> " + sadRemoved);
-                //System.out.println("ads.removeFirst() -> " + adsRemoved);
-                //System.out.println();
-                assertEquals("Mismatch at removeLast()", adsRemoved, sadRemoved);
+                operationSequence += "removeLast()\n";
+                assertEquals(operationSequence, adsRemoved, sadRemoved);
             }
         }
 
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         jh61b.junit.TestRunner.runTests(TestArrayDequeEC.class);
-    }
+    }*/
+
+
 }
