419dfc5c51a7eedf8fe1dc2c95192981e324af88
Achilles <jiangyongjian01@gmail.com>
2024-06-22 16:28:42 +0800

    重写max()，而不是用Math.max()代替

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -8,14 +8,18 @@ import java.util.Arrays;
  */
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
-    public static int max(int a, int b) {
-        int w = (b - a) >> 31;
+
+    //public static int max(int a, int b) {
+        //int w = (b - a) >> 31;
         /* If you're stepping into this function, click the
            step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
+        //int z = ~(b - a) >> 31;
 
-        int max = b & w | a & z;
-        return max;
+        //int max = b & w | a & z;
+        //return max;
+    //}
+    public static int max(int a, int b) {
+        return Math.max(a, b);
     }
 
 
@@ -48,7 +52,7 @@ public class DebugExercise2 {
         }
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = Math.max(a[i], b[i]);
+            int biggerValue = max(a[i], b[i]);
             returnArray[i] = biggerValue;
         }
 
