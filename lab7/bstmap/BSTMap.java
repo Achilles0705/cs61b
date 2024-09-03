94af4141edf17453be7121f1ab6214fe48fc3361
Achilles <jiangyongjian01@gmail.com>
2024-09-03 10:55:04 +0800

    完成lab7(除äOptional Exercises)

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,100 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private BSTNode root;
+
+    private class  BSTNode {
+        private K key;
+        private V val;
+        private BSTNode left, right;
+        private int size;
+
+        public BSTNode(K key, V val, int size) {
+            this.key = key;
+            this.val = val;
+            this.size = size;
+        }
+    }
+
+    @Override
+    public void clear() {
+        root = clear(root);
+    }
+
+    public BSTNode clear(BSTNode node) {
+        if (node == null) {
+            return null;
+        }
+        node.left = clear(node.left);
+        node.right = clear(node.right);
+        node = null;
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return get(key) != null;
+    }
+
+    public V get(BSTNode x, K key) {
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x.val;
+    }
+
+    @Override
+    public V get(K key) {
+        return get(root ,key);
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    private int size(BSTNode x) {
+        if (x == null) return 0;
+        else return x.size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    public BSTNode put(BSTNode x, K key, V val) {
+        if (x == null) return new BSTNode(key, val ,1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+}
